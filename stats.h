98537053f2526eb142be825d16fc318f52169d5f
univac <68467947+univac-os@users.noreply.github.com>
2020-09-01 13:06:15 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,49 @@
 #include <vector>
+#include<bits/stdc++.h>
 
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<___>& );
+    //use template cause we use primitive data type
+    template <class T>
+    struct stat
+    {
+        T avg;
+        T max;
+        T min;
+    };
+    
+    template <typename T>
+    void ComputeAverage(const vector<T>& data,struct stat<T>& computeValue)
+    {
+        //take value from vector and show average through strcut
+        T sum=0;
+        sum=accumlate(data.begin(),data.end(),0);
+        computeValue.avg=(sum/data.size());
+    }
+    
+    template <typename T>
+    void ComputeMax(const vector<T>& data,struct stat<T>& computeValue)
+    {
+        //take value from vector and show max through strcut
+        T max=0;
+        max=*max_element(data.begin(),data.end());
+        computeValue.max=max;
+    }
+    
+    template <typename T>
+    void ComputeMin(const vector<T>& data,struct stat<T>& computeValue)
+    {
+        //take value from vector and show min through strcut
+        T min=0;
+        min=*min_element(data.begin(),data.end());
+        computeValue.max=max;
+    }
+    
+    template<typename T>
+    void stat<T> ComputeStatistics(struct computeValue<T>& computeValue,const vector<T>& data)
+    {
+        computeAverage(computeValue,data);
+        computeMax(computeValue,data);
+        computeMin(computeValue,data);
+    }
 }
