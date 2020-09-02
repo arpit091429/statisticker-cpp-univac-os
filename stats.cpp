4e244920cafd2ce2e6c414fdde49697feeaea44f
univac <68467947+univac-os@users.noreply.github.com>
2020-09-02 17:55:48 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,20 @@
 #include "stats.h"
+#include<cmath>
+#include<bits/stdc++.h>
 
-//Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
-
+template<typenmae T> Statisitcs::Stat::Stat(){}
+template<typenmae T> Statisitcs::Stat<T,T,T>::Stat(T average,T Max,T min):average(average),Max(Max),Min(Min){}
+template<typename T> Statistics::Stat Statistics::ComputeStatistics(const std::vector<T>& data)
+{
+    if(data.size()==0)
+    {
+        return Stat(NaN,NaN,NaN);
+    }
+    
+    T maxi=*max_element(data.begin(),data.end());
+    T mani=*min_element(data.begin(),data.end());
+    T sum=accumulate(data.begin(), data.end() , 0);
+    T avg=sum/data.size();
+    return Statistics::Stat(avg,maxi,mini);
+    
+}
