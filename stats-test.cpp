510ffcedc7fccab8d36a1f18187eb0c522802e01
univac <68467947+univac-os@users.noreply.github.com>
2020-09-02 17:34:40 +0530

    Update stats-test.cpp
    another method

--- stats-test.cpp
+++ stats-test.cpp
@@ -3,7 +3,7 @@
 #include "catch.hpp"
 #include "stats.h"
 #include <cmath>
-TEST_CASE("reports average, minimum and maximum") {
+/*TEST_CASE("reports average, minimum and maximum") {
 	Statistics::Stat<float> computedvalues;
     Statistics::Stat<float> computedstats = Statistics::ComputeStatistics(computedvalues,{1.5, 8.9, 3.1, 4.5});
     float epsilon = 0.001;
@@ -15,6 +15,22 @@ TEST_CASE("reports average, minimum and maximum") {
 TEST_CASE("average is NaN for empty array") {
 	 Statistics::Stat<float> computedvalues;
     Statistics::Stat<float> computedstats = Statistics::ComputeStatistics(computedvalues, {});
+    REQUIRE(isnan(computedstats.average));
+    REQUIRE(isnan(computedstats.Max));
+    REQUIRE(isnan(computedstats.Min));
+}
+*/
+//trying another method
+TEST_CASE("reports average, minimum and maximum") {
+    auto computedStats=Statistics::ComputeStatistics({1.5, 8.9, 3.1, 4.5});
+    float epsilon = 0.001;
+    REQUIRE(std::abs(computedstats.average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedstats.Max - 8.9) < epsilon);
+    REQUIRE(std::abs(computedstats.Min - 1.5) < epsilon);
+}
+
+TEST_CASE("average is NaN for empty array") {
+    auto computedstats = Statistics::ComputeStatistics(computedvalues, {});
     REQUIRE(isnan(computedstats.average));
     REQUIRE(isnan(computedstats.Max));
     REQUIRE(isnan(computedstats.Min));
