671229756ff6c42151cfb7f1e1c788f7a8c24a70
univac <68467947+univac-os@users.noreply.github.com>
2020-09-02 18:19:20 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -24,14 +24,14 @@ TEST_CASE("average is NaN for empty array") {
 TEST_CASE("reports average, minimum and maximum") {
     auto computedStats=Statistics::ComputeStatistics({1.5, 8.9, 3.1, 4.5});
     float epsilon = 0.001;
-    REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.Average - 4.525) < epsilon);
     REQUIRE(std::abs(computedStats.Max - 8.9) < epsilon);
     REQUIRE(std::abs(computedStats.Min - 1.5) < epsilon);
 }
 
 TEST_CASE("average is NaN for empty array") {
     auto computedStats = Statistics::ComputeStatistics({});
-    REQUIRE(isnan(computedStats.average));
+    REQUIRE(isnan(computedStats.Average));
     REQUIRE(isnan(computedStats.Max));
     REQUIRE(isnan(computedStats.Min));
 }
