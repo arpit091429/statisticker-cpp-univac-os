3f48535d0b6a4ff9b8313b47ccf15807061eb58f
univac <68467947+univac-os@users.noreply.github.com>
2020-09-02 11:04:56 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -13,9 +13,9 @@ TEST_CASE("reports average, minimum and maximum") {
     Statistics:: stat<float> ComputedStats=Statistics:: ComputeStatistics(data,computedValue);
     //auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     float epsilon = 0.001;
-    REQUIRE(std::abs(ComputedStats.average - 4.525) < epsilon);
-    REQUIRE(std::abs(ComputedStats.max - 8.9) < epsilon);
-    REQUIRE(std::abs(ComputedStats.min - 1.5) < epsilon);
+    REQUIRE(std::abs(ComputedStats.Avg - 4.525) < epsilon);
+    REQUIRE(std::abs(ComputedStats.Max - 8.9) < epsilon);
+    REQUIRE(std::abs(ComputedStats.Min - 1.5) < epsilon);
 }
 
 TEST_CASE("average is NaN for empty array") {
@@ -25,8 +25,8 @@ TEST_CASE("average is NaN for empty array") {
     //auto computedStats = Statistics::ComputeStatistics({});
     //All fields of computedStats (average, max, min) must be
     //Use http://www.cplusplus.com/reference/cmath/isnan/
-    REQUIRE(std::isnan(ComputedStats.average));
-    REQUIRE(std::isnan(ComputedStats.max));
-    REQUIRE(std::isnan(ComputedStats.min));
+    REQUIRE(std::isnan(ComputedStats.Avg));
+    REQUIRE(std::isnan(ComputedStats.Max));
+    REQUIRE(std::isnan(ComputedStats.Min));
     
 }
