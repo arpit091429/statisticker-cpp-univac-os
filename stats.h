337f26e45e2b007bac447665b5b0441df6fee7f5
univac <68467947+univac-os@users.noreply.github.com>
2020-09-02 11:13:34 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,50 +1,46 @@
+#include<iostream>
 #include <vector>
+#include<math.h>
 #include<bits/stdc++.h>
+using namespace std;
 
-namespace Statistics {
-    //Stats ComputeStatistics(const std::vector<___>& );
-    //use template cause we use primitive data type
-    template <typename T>
-    struct stat
-    {
-        T Avg;
-        T Max;
-        T Min;
-    };
+namespace Statistics
+{
     
-    template <typename T>
-    void ComputeAverage(const vector<T>& data,struct stat<T>& computeValue)
+	template <class T>
+	struct Stat
     {
-        //take value from vector and show average through strcut
-        T Sum=0; 
-        Sum=accumulate( data.begin(), data.end(),0);
-        computeValue.avg=(Sum/data.size());
-    }
+		T average;
+		T Max;
+		T Min;
+	};	
     
-    template <typename T>
-    void ComputeMax(const vector<T>& data,struct stat<T>& computeValue)
+	template <typename T>
+	void ComputeAverage(struct Stat<T> &computedValues, const vector<T>& data)
     {
-        //take value from vector and show max through strcut
-        T Max=0;
-        Max=*max_element( data.begin(), data.end());
-        computeValue.Max=Max;
-    }
+		T sum = accumulate( data.begin(), data.end(), 0);
+		computedValues.average = (sum / data.size());
+	}
+	
+	template <typename T>
+	void ComputeMax(struct Stat<T> &computedValues, const vector<T>& data)
+    {
+		computedValues.Max = *max_element(data.begin(), data.end());
+	}
+	
+	template <typename T>
+	void ComputeMin(struct Stat<T> &computedValues, const vector<T>& data)
+    {
+		computedValues.Min = *min_element(data.begin(), data.end());
+	}
+	
+	template <typename T>
+	struct Stat<T> ComputeStatistics(struct Stat<T> &computedValues, const vector<T>& data)
+    {
+		ComputeAverage(computedValues,data);
+		ComputeMax(computedValues,data);
+		ComputeMin(computedValues,data);
+		return computedValues;
+	}
     
-    template <typename T>
-    void ComputeMin(const vector<T>& data,struct stat<T>& computeValue)
-    {
-        //take value from vector and show min through strcut
-        T Min=0;
-        Min=*min_element( data.begin(), data.end());
-        computeValue.Min=Min;
-    }
-    
-    template<typename T>
-    void stat<T> ComputeStatistics(const vector<T>& data,struct stat<T>& computeValue)
-    {
-        ComputeAverage(data,computeValue);
-        ComputeMax(data,computeValue);
-        ComputeMin(data,computeValue);
-        return computeValue;
-    }
 }
