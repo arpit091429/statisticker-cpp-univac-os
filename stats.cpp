a673a77629b66fea9845a30ef803d0493e563941
univac <68467947+univac-os@users.noreply.github.com>
2020-09-03 14:36:51 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,9 +2,9 @@
 #include<cmath>
 #include<bits/stdc++.h>
 
-template<typename T> Statistics::Stat::Stat(){}
-template<typename T> Statistics::Stat<T,T,T>::Stat(T average,T Max,T min):average(average),Max(Max),Min(Min){}
-template<typename T> Statistics::Stat<T> Statistics::ComputeStatistics(const std::vector<T>& data)
+template<typename T> Statistics::Stats::Stats(){}
+template<typename T> Statistics::Stats<T,T,T>::Stats(T average,T Max,T min):average(average),Max(Max),Min(Min){}
+template<typename T> Statistics::Stats<T> Statistics::ComputeStatistics(const std::vector<T>& data)
 //Statistics::Stats::Stats(){}
 //Statistics::Stats::Stats(float Average,float Max,float Min):Average(Average),Max(Max),Min(Min){}
 //Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data)
@@ -19,12 +19,12 @@ template<typename T> Statistics::Stat<T> Statistics::ComputeStatistics(const std
     T mani=*min_element(data.begin(),data.end());
     T sum=accumulate(data.begin(), data.end() , 0);
     T avg=(sum/data.size());
-    return Statistics::Stat(avg,maxi,mini);
+    return Statistics::Stats(avg,maxi,mini);
    // float maxi=*max_element(data.begin(),data.end());
     //float mini=*min_element(data.begin(),data.end());
     //float sum=accumulate(data.begin(), data.end() , 0);
     //float avg=sum/data.size();
-    return Statistics::Stats(avg,maxi,mini);
+    //return Statistics::Stats(avg,maxi,mini);
 
     
 }
