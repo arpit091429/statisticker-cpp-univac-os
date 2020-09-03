1140ef74df109da75bf8d03965f786cc7eaa8b27
univac <68467947+univac-os@users.noreply.github.com>
2020-09-03 14:30:03 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -8,22 +8,23 @@ using namespace std;
 namespace Statistics
 {
     
-	//template <typename T=double>	//default
+	template <typename T=double>	//default
 	class Stats
    	 {
 	   public:
-		//T average;
-		//T Max;
-		//T Min;
-		float Average;
-		float Max;
-		float Min;
+		T average;
+		T Max;
+		T Min;
+		//float Average;
+		//float Max;
+		//float Min;
 		Stats();//empty array
-		Stats(float avg,float max,float min);
+		//Stats(float avg,float max,float min);
+		Stats(T average,T Max,T Min);
 	};
-	//template<typename T=double>
-	//Stat ComputeStatistics(const std::vector<T>& );
-	Stats ComputeStatistics(const std::vector<float>&);
+	template<typename T=double>
+	Stat<T> ComputeStatistics(const std::vector<T>& );
+	//Stats ComputeStatistics(const std::vector<float>&);
 }
 //decalartion
 	
