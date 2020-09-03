5a1f01c2b9a1843b91b515d601367b990e082b56
univac <68467947+univac-os@users.noreply.github.com>
2020-09-03 14:34:07 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,12 +2,12 @@
 #include<cmath>
 #include<bits/stdc++.h>
 
-//template<typename T> Statistics::Stat::Stat(){}
-//template<typename T> Statistics::Stat<T,T,T>::Stat(T average,T Max,T min):average(average),Max(Max),Min(Min){}
-//template<typename T> Statistics::Stat Statistics::ComputeStatistics(const std::vector<T>& data)
-Statistics::Stats::Stats(){}
-Statistics::Stats::Stats(float Average,float Max,float Min):Average(Average),Max(Max),Min(Min){}
-Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data)
+template<typename T> Statistics::Stat::Stat(){}
+template<typename T> Statistics::Stat<T,T,T>::Stat(T average,T Max,T min):average(average),Max(Max),Min(Min){}
+template<typename T> Statistics::Stat<T> Statistics::ComputeStatistics(const std::vector<T>& data)
+//Statistics::Stats::Stats(){}
+//Statistics::Stats::Stats(float Average,float Max,float Min):Average(Average),Max(Max),Min(Min){}
+//Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data)
 
 {
     if(data.size()==0)
@@ -15,15 +15,15 @@ Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data)
         return Stats(NAN,NAN,NAN);
     }
     
-    //T maxi=*max_element(data.begin(),data.end());
-    //T mani=*min_element(data.begin(),data.end());
-    //T sum=accumulate(data.begin(), data.end() , 0);
-    //T avg=sum/data.size();
-    //return Statistics::Stat(avg,maxi,mini);
-    float maxi=*max_element(data.begin(),data.end());
-    float mini=*min_element(data.begin(),data.end());
-    float sum=accumulate(data.begin(), data.end() , 0);
-    float avg=sum/data.size();
+    T maxi=*max_element(data.begin(),data.end());
+    T mani=*min_element(data.begin(),data.end());
+    T sum=accumulate(data.begin(), data.end() , 0);
+    T avg=(sum/data.size());
+    return Statistics::Stat(avg,maxi,mini);
+   // float maxi=*max_element(data.begin(),data.end());
+    //float mini=*min_element(data.begin(),data.end());
+    //float sum=accumulate(data.begin(), data.end() , 0);
+    //float avg=sum/data.size();
     return Statistics::Stats(avg,maxi,mini);
 
     
